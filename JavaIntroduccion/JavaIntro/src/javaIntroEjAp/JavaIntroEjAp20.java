6a2b5c0868c9b8e57dfa1f1b62bcf5fc33a6699d
LaureanoCid <laurycid@gmail.com>
2023-04-27 19:15:00 -0300

    Java Introduccion

---
+++ JavaIntroduccion/JavaIntro/src/javaIntroEjAp/JavaIntroEjAp20.java
@@ -0,0 +1,66 @@
+/*
+ Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 donde la
+suma de sus filas, sus columnas y sus diagonales son idénticas. Crear un programa que
+permita introducir un cuadrado por teclado y determine si este cuadrado es mágico o no.
+El programa deberá comprobar que los números introducidos son correctos, es decir,
+están entre el 1 y el 9.
+ */
+package javaIntroEjAp;
+import java.util.Scanner;
+
+public class JavaIntroEjAp20 {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int[][] matriz=new int [3][3];
+        llenado(matriz);
+        comprobar(matriz);
+    }
+    
+    //Llenar la matriz
+    public static int[][] llenado(int[][] matriz){
+        Scanner leer = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("de un valor entre 1 y 9");
+                matriz[i][j]=leer.nextInt();
+                if(matriz[i][j]<1 || matriz[i][j]>9){
+                    do {                        
+                        System.out.println("de un valor entre 1 y 9");
+                        matriz[i][j]=leer.nextInt();
+                    } while (matriz[i][j]<1 || matriz[i][j]>9);
+                }
+            }                          
+        }
+        return matriz;
+    }
+    
+    public static void comprobar(int[][] matriz){
+        int sumaH=0, sumaV=0,sumaD=0,contH=0,contV=0;       
+        //verificar la suma 
+        for (int i = 0; i < 3; i++) {
+            sumaH=0;
+            sumaV=0;
+            for (int j = 0; j < 3; j++) {
+                //suma horizontal
+                sumaH=sumaH+matriz[i][j];
+                if (sumaH==15){
+                    contH++;
+                }
+                //suma vertical
+                sumaV=sumaV+matriz[j][i];
+                if(sumaV==15){
+                    contV++;
+                }
+            }
+        }
+        for (int k = 0; k < 3; k++) {
+            sumaD=sumaD+matriz[k][k];
+        }        
+        if(contH==3 && contV==3 && sumaD==15){
+            System.out.println("la matriz es mágica");
+        }else{
+            System.out.println("la matriz No es mñagica");
+        }
+    }   
+}
---
+++ JavaIntroduccion/JavaIntro/src/javaIntroEjAp/JavaIntroEjAp21.java
@@ -0,0 +1,80 @@
+/*
+Dadas dos matrices cuadradas de números enteros, la matriz M de 10x10 y la matriz P de
+3x3, se solicita escribir un programa en el cual se compruebe si la matriz P está contenida
+dentro de la matriz M. Para ello se debe verificar si entre todas las submatrices de 3x3
+que se pueden formar en la matriz M, desplazándose por filas o columnas, existe al
+menos una que coincida con la matriz P. En ese caso, el programa debe indicar la fila y la
+columna de la matriz M en la cual empieza el primer elemento de la submatriz P.
+Ejemplo:
+
+Como podemos observar nuestra submatriz P se encuentra en la matriz M en los índices:
+4,4 - 4,5 - 4,6 - 5,4 - 5,5 - 5,6 - 6,4 - 6,5 - 6,6.
+ */
+package javaIntroEjAp;
+import java.util.Scanner;
+
+public class JavaIntroEjAp21 {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int[][] matri10=new int [10][10];
+        boolean comp = false;
+       
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                matri10[i][j]=(int)(Math.random()*10);
+            }
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+            for (int j = 0; j < 10; j++) {
+                System.out.print("["+matri10[i][j]+"]");
+            }
+        }
+        
+        int[][] matri3= new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("ingrese un valor");
+                matri3[i][j]=leer.nextInt();
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if(matri10[i][j]==matri3[0][0]){
+                    int aux1=i,aux2=j;
+                    comparar(matri10,matri3,aux1,aux2,comp);
+                }
+                if(comp=true){
+                    break;
+                }
+            }
+            if(comp=true){
+                System.out.println("se encontró la matriz");
+                break;
+            }
+        }
+        if(comp=false){
+            System.out.println("NO se ha enocntrado la matriz");
+        }     
+    }
+    
+    public static boolean comparar(int[][]matri10, int[][] matri3,int aux1,int aux2, boolean comp){
+        int k=0,l=0;
+        for (int i = aux1; i < aux1+3; i++) {
+            l=0;
+            for (int j = aux2; j < aux2+3; j++) {
+                if (matri10[i][j]==matri3[k][l]) {
+                    comp=true;
+                }else{
+                    comp=false;
+                    break;
+                }
+                l++;
+            }
+            k++;
+        }        
+        return comp;
+    }                   
+}
