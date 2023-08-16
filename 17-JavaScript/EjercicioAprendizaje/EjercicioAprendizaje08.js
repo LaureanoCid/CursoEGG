60c367eb9bb616d77f6055427cb12e25e4cde310
LaureanoCid <laurycid@gmail.com>
2023-08-15 21:38:55 -0300

    Actualizado

---
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje08.js
@@ -0,0 +1,15 @@
+/*
+    Escribir un programa que lea números enteros hasta teclear 0 (cero). Al finalizar el 
+    programa se debe mostrar el máximo número ingresado, el mínimo, y el promedio de 
+    todos ellos. 
+*/
+
+var opcion = 0, min, max, prom, cont;
+
+opcion = prompt("Ingrese un numero: ");
+min = opcion;
+max = opcion;
+cont++;
+while (opcion != 0) {
+    
+}
