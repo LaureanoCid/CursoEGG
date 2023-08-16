ab4d0b322aa0cd768b1c47fab7cfc046fe0f2813
LaureanoCid <laurycid@gmail.com>
2023-08-15 21:44:34 -0300

    Hecho 14/08/2023

--- 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje01.js
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje01.js
@@ -3,5 +3,5 @@
     hoy? (soleado, nublado, lloviendo). A continuación, mostrar por pantalla un mensaje que 
     indique “El día de hoy está ...”, completando el mensaje con el dato que ingresó el usuario. 
 */
-var resp = prompt("Como esta el dia hoy? Soleado - Nublado - Lluvioso");
+var resp = prompt("Como esta el dia hoy? Soleado - Nublado - Lluvioso...");
 alert("Hoy es un dia " + resp);
