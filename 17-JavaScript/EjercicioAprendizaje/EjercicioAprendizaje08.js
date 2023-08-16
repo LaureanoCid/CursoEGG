8bde3fe5216b4ed3a5a83045599293bdffe051a7
LaureanoCid <laurycid@gmail.com>
2023-08-15 23:55:10 -0300

    Hecho 15/08/2023

--- 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje08.js
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje08.js
@@ -4,12 +4,24 @@
     todos ellos. 
 */
 
-var opcion = 0, min, max, prom, cont;
+var opcion = 0, prom = 0, cont = 0;
 
-opcion = prompt("Ingrese un numero: ");
-min = opcion;
-max = opcion;
+opcion = parseInt(prompt("Ingrese un numero: "));
+var min = opcion;
+var max = opcion;
 cont++;
+prom = opcion;
 while (opcion != 0) {
-    
+    opcion = parseInt(prompt("Ingrese un numero: "));
+    if (opcion != 0) {
+        (opcion<min)? min = opcion: min = min;
+        (opcion>max)? max = opcion: max = max;
+        cont++;
+        prom += opcion;
+    }    
 }
+prom = prom / cont;
+var msj = `El numero maximo es ${max}
+El numero minimo es ${min}
+y el promedio es ${prom}`
+alert(msj);
---
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje10.js
@@ -0,0 +1,9 @@
+/*
+    Escribir una función flecha que reciba una palabra y la devuelva al revés.
+*/
+var palabra = prompt("Ingrese una palabra: ");
+reves(palabra);
+
+
+
+let reves = (palabra) => alert()
