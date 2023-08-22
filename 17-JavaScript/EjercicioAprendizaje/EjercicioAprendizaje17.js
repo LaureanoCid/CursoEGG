4b00617e19077dcfa89052613b7c2b55c10f73cc
LaureanoCid <laurycid@gmail.com>
2023-08-21 22:51:11 -0300

    Hecho 21/08/2023

---
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje17.js
@@ -0,0 +1,17 @@
+/*
+    Realizar un programa que elimine los dos últimos elementos de un array. Mostrar el 
+    resultado
+*/
+alert("Bienvenido al creador de arrays");
+let longitud = prompt("Ingrese la cantidad de palabras que desea ingresar: ");
+let array1 = [];
+
+for (let index = 0; index < longitud; index++) {
+    array1[index] = prompt("Ingrese una palabra: ");
+}
+alert(`Contunto inicial: ${array1}`);
+
+delete array1[array1.length-1];
+delete array1[array1.length-2];
+
+alert(`Conjunto final: ${array1}`);
