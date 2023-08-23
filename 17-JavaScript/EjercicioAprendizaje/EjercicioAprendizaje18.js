5cdb0fd71aac4d9c9d9d4d5e26ff135421e8ac2d
LaureanoCid <laurycid@gmail.com>
2023-08-22 23:17:23 -0300

    Hecho 22/08/2023

--- 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje18.js
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje18.js
@@ -8,18 +8,28 @@
 */
 let array1 = [true, 5, false, "hola", "adios", 2];
 let palabra = "";
+let contB = 0, num1 = 0, num2 = 0;
+let msj = ""
 
 alert("Bienvenido al array loco :)");
 
 for (let i = 0; i < array1.length; i++) {
-    alert(typeof(array1[i]));
-    if (typeof(array1[i]) == String) {
+    if (typeof array1[i] == "string") {
         palabra = (array1[i].length > palabra.length)?array1[i]:palabra;
     }    
+    (typeof array1[i] == "boolean")?contB++:contB;
+    ((typeof array1[i] == "number")&&(num1 == 0))?num1=array1[i]:((typeof array1[i] == "number")&&(num2 == 0))?num2=array1[i]:num2;
 }
+(contB <= 2)?msj="Hay 2 boleanos en el Array":msj+="No hay 2 boleanos en el array";
+(contB > 2)?msj+=" por ende hay mas de 2 boleanos.":msj+=" por ende no hay mas de 2 boleanos en el array.";
 
 alert(`
 A) ${palabra}
-B)
-C)
+B) ${msj}
+C)  Operaciones: 
+Suma: ${num1 + num2}
+Resta: ${num1 - num2}
+Multiplicacion: ${num1 * num2}
+Division: ${num1 / num2}
+Potencia: ${Math.pow(num1,num2)}
 `);
