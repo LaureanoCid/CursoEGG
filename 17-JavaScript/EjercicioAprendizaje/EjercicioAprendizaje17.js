c1c6a070a33e1b73ef4824f73687802a92861046
LaureanoCid <laurycid@gmail.com>
2023-08-22 22:32:13 -0300

    Hecho 21/08/2023

--- 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje17.js
+++ 17-JavaScript/EjercicioAprendizaje/EjercicioAprendizaje17.js
@@ -10,8 +10,10 @@ for (let index = 0; index < longitud; index++) {
     array1[index] = prompt("Ingrese una palabra: ");
 }
 alert(`Contunto inicial: ${array1}`);
+//Borrado con DELETE
+// delete array1[array1.length-1];
+// delete array1[array1.length-2];
 
-delete array1[array1.length-1];
-delete array1[array1.length-2];
+array1.splice(array1.length-2,2);
 
 alert(`Conjunto final: ${array1}`);
